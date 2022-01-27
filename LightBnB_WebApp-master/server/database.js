92589e66e62515438c5a0684e9b5417106a2ebbe
Alison Harman <alison_harman@sympatico.ca>
2022-01-27 20:11:32 +0000

    refactor getAllProperties function to accept arguments to filter results

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -153,10 +153,69 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
 
+  const queryParams = [];
+  
+  let queryString =`SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    if (queryParams.length === 1) {
+      queryString += `WHERE owner_id = $${queryParams.length} `;
+    }
+    else {
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    const costPerNight = parseFloat(options.minimum_price_per_night)*100;
+    queryParams.push(costPerNight);
+    if (queryParams.length === 1) {
+      queryString += `WHERE cost_per_night > $${queryParams.length} `
+    }
+    else {
+      queryString += `AND cost_per_night > $${queryParams.length} `
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    const costPerNight = parseFloat(options.maximum_price_per_night)*100;
+    queryParams.push(costPerNight);
+    if (queryParams.length === 1) {
+      queryString += `WHERE cost_per_night < $${queryParams.length} `
+    }
+    else {
+      queryString += `AND cost_per_night < $${queryParams.length} `
+    }
+  }
+
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`
-    SELECT * FROM properties
-    LIMIT $1`, [limit] )
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
@@ -173,7 +232,7 @@ const getAllProperties = function(options, limit = 10) {
   */
 }
 
-getAllProperties();
+//getAllProperties();
 exports.getAllProperties = getAllProperties;
 
 
