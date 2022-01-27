c20f6989226cba56fc1335458f5b41c07527f45f
Alison Harman <alison_harman@sympatico.ca>
2022-01-27 15:41:51 +0000

    refactor function of get all user reservations to query database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,11 +70,9 @@ const getUserWithId = function(id) {
     .then((result) => {
       const userObject = result.rows[0];
       if (userObject) {
-        console.log('ID function', userObject);
         return userObject;
       }
       else {
-        console.log('am i here?')
         return null;
       }
     })
@@ -127,7 +125,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT 10;
+    `, [guest_id])
+    .then(res => {
+      return res.rows;
+    })
 }
 exports.getAllReservations = getAllReservations;
 
